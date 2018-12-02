6c4fba04e66d9de7dcf803e153b9dcbdf689f0c5
Keir Forster <keirforster@gmail.com>
2018-12-01 18:44:23 -0800

    Create model classes for login and register data

---
+++ src/app/services/models/register-model.ts
@@ -0,0 +1,9 @@
+export class RegisterModel {
+  constructor(
+    public email: string,
+    public firstName: string,
+    public lastName: string,
+    public userName: string,
+    public password: string
+  ) {}
+}
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,94 +1,96 @@
-import { Injectable } from '@angular/core';
+// Angular modules
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+
+// App
 import { ApiurlService } from '../services/apiurl.service';
+import { LoginModel } from './models/login-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
-  public JWTToken = "";
-  public expire = "";
+  public JWTToken = '';
+  public expire = '';
   public loggedIn = false;
   public userData: any;
-  public uid = "";
-  private jwtType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+  public uid = '';
+  private jwtType =
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
   public role;
-  constructor(private router: Router, private http: HttpClient, private ApiUrl: ApiurlService) {
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private ApiUrl: ApiurlService
+  ) {}
 
-  }
-
-  login(loginModel) {
-    let data = loginModel;
+  login(loginModel: LoginModel) {
     var config = {
       headers: {
-        "Content-Type": "application/json; charset = utf-8;"
+        'Content-Type': 'application/json; charset = utf-8;'
       }
     };
-    this.http.post(this.ApiUrl.loginUrl, data, config)
-      .subscribe(
-        (res) => {
-          this.JWTToken = res["token"];
-          localStorage.setItem('LoggedJWT', this.JWTToken);
-          this.expire = res["expiration"];
-          this.decodeToken(res['token']);
-          this.loggedIn = true;
-          this.router.navigate(['/dashboard']);
-        },
-        err => {
-          console.log(err);
-        }
-      );
+    this.http.post(this.ApiUrl.loginUrl, loginModel, config).subscribe(
+      res => {
+        this.JWTToken = res['token'];
+        localStorage.setItem('LoggedJWT', this.JWTToken);
+        this.expire = res['expiration'];
+        this.decodeToken(res['token']);
+        this.loggedIn = true;
+        this.router.navigate(['/dashboard']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   registerStudent(registerStudentModel) {
     let data = registerStudentModel;
     var config = {
       headers: {
-        "Content-Type": "application/json; charset = utf-8;"
+        'Content-Type': 'application/json; charset = utf-8;'
       }
     };
-    this.http.post(this.ApiUrl.registerStudentUrl, data, config)
-      .subscribe(
-        (res) => {
-          console.log(res);
-          this.router.navigate(['/login']);
-        },
-        err => {
-          console.log(err);
-        }
-      );
+    this.http.post(this.ApiUrl.registerStudentUrl, data, config).subscribe(
+      res => {
+        console.log(res);
+        this.router.navigate(['/login']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   registerTeacher(registerStudentModel) {
     let data = registerStudentModel;
     var config = {
       headers: {
-        "Content-Type": "application/json; charset = utf-8;"
+        'Content-Type': 'application/json; charset = utf-8;'
       }
     };
-    this.http.post(this.ApiUrl.registerTeacherUrl, data, config)
-      .subscribe(
-        (res) => {
-          console.log(res);
-          this.router.navigate(['/login']);
-        },
-        err => {
-          console.log(err);
-        }
-      );
+    this.http.post(this.ApiUrl.registerTeacherUrl, data, config).subscribe(
+      res => {
+        console.log(res);
+        this.router.navigate(['/login']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-
 
   decodeToken(token) {
     let jwt = token;
-    let jwtData = jwt.split('.')[1]
-    let decodedJwtJsonData = window.atob(jwtData)
-    let decodedJwtData = JSON.parse(decodedJwtJsonData)
+    let jwtData = jwt.split('.')[1];
+    let decodedJwtJsonData = window.atob(jwtData);
+    let decodedJwtData = JSON.parse(decodedJwtJsonData);
     console.log(decodedJwtData);
     if (decodedJwtData != null) {
-      this.role = decodedJwtData["roles"];
-      this.uid = decodedJwtData["uid"];
+      this.role = decodedJwtData['roles'];
+      this.uid = decodedJwtData['uid'];
       console.log(this.role);
     }
   }
